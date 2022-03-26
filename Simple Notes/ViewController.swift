53134dcb100d1e83345ad4680b065cca8c7afa2e
pgat3000 <pgat3000@gmail.com>
2022-03-26 15:34:03 -0600

    Initial Commit

---
+++ Simple Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Notes
+//
+//  Created by Pablo Gatgens on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
